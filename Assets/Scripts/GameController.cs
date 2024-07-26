6719c7d1a03429aa7554bc2eb828203f5624f844
Dan <dantroon@gmail.com>
2024-07-25 17:49:23 -0700

    Scrolling terrain based on forward velocity

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+	[SerializeField]
+	private PlayerCharacter _player;
+	[SerializeField]
+	private List<MeshRenderer> _scrollingTerrainMeshes;
 
+	private const float _terrainSpeedScalar = 0.1f;
+
+	private void Update() {
+		foreach (MeshRenderer mesh in _scrollingTerrainMeshes) {
+			mesh.material.mainTextureOffset += new Vector2(0f, -_player.forwardVelocity * Time.deltaTime * _terrainSpeedScalar);
+		}
+	}
 }
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -6,11 +6,17 @@ public class PlayerCharacter : MonoBehaviour {
 	private static KeyCode backKey => KeyCode.S;
 	private static KeyCode forwardKey => KeyCode.W;
 
-	private float _forwardVelocity = 0f;
+	private float _forwardVelocity = .2f;
 	private float _horizontalVelocity = 0f;
 
-	private float _maxForwardVelocity = 8f;
-	private float _maxHorizontalVelocity = 2f;
+	private float _forwardAcceleration = .4f;
+
+	private float _forwardVelocityMin = .2f;
+	private float _forwardVelocityMax = 12f;
+	private float _horizontalVelocityMax = 2f;
+
+	public float forwardVelocity => _forwardVelocity;
+	public float horizontalVelocity => _horizontalVelocity;
 
 	private void Update() {
 		float horizontalFactor = 0f;
@@ -26,7 +32,11 @@ public class PlayerCharacter : MonoBehaviour {
 		if (Input.GetKey(backKey)) {
 			//Brake
 		} else {
+			if (_forwardVelocity < _forwardVelocityMax) {
+				_forwardVelocity = Mathf.Max(_forwardVelocity + _forwardAcceleration * Time.deltaTime, _forwardVelocityMax);
+			}
 
 		}
+
 	}
 }
