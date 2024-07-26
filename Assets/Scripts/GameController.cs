202b15663d0fdfd1041e10f775d1f90a9ba13f6b
Dan <dantroon@gmail.com>
2024-07-26 13:40:19 -0700

    Travel distance tracking

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,8 @@ public class GameController : MonoBehaviour {
 
 	private const float _terrainSpeedScalar = 0.1f;
 
+	public float distanceTraveled { get; private set; } = 0f;
+
 	private void Awake() {
 		DontDestroyOnLoad(gameObject);
 	}
@@ -18,13 +20,17 @@ public class GameController : MonoBehaviour {
 	}
 
 	private void Update() {
-		foreach (MeshRenderer mesh in _scrollingTerrainMeshes) {
-			mesh.material.mainTextureOffset += new Vector2(0f, -_player.forwardVelocity * Time.deltaTime * _terrainSpeedScalar);
+		if (_player.movementEnabled) {
+			foreach (MeshRenderer mesh in _scrollingTerrainMeshes) {
+				mesh.material.mainTextureOffset += new Vector2(0f, -_player.forwardVelocity * Time.deltaTime * _terrainSpeedScalar);
+			}
+			distanceTraveled += _player.forwardVelocity * Time.deltaTime;
 		}
 	}
 
 	private void StartLevel() {
 		_player.movementEnabled = true;
+		distanceTraveled = 0f;
 	}
 
 	private void EnterShop() {
