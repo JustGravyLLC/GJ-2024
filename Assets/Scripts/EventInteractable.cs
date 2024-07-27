227f9e0d4c7ec6a84f5e3267a176b7ae02ae966d
flamejackal <justgravyllc@gmail.com>
2024-07-26 19:41:30 -0500

    Check speed instead of timer for Event Interactable

--- Assets/Scripts/EventInteractable.cs
+++ Assets/Scripts/EventInteractable.cs
@@ -5,28 +5,26 @@ using UnityEngine;
 public class EventInteractable : Interactable
 {
     private bool interacted = false;
-    private float timer = 2f;
-    private bool runTime = false;
+    private bool inside = false;
+    private float _minSpeed = 5f;
 
     public override void OnInteract()
     {
-        //Count time to interact
-        runTime = true;
+        inside = true;
     }
 
     public override void OnLeave()
     {
-        if (runTime) runTime = false;
+        inside = false;
     }
 
     private void Update()
     {
         if (interacted) return;
 
-        //if timer is running, decrement
-        if (runTime){timer -= Time.deltaTime;}
+        if (!inside) return;
 
-        if(timer < 0)
+        if(_gameController.playerCharacter.forwardVelocity < _minSpeed)
         {
             interacted = true;
             _gameController.EnterEvent();
